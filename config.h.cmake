1eed091df23eef1d2432ce30e181262bfe180737
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-12-12 17:06:54 -0500

    Add Build namespace

---
+++ config.h.cmake
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#cmakedefine DATADIR "@DATADIR@"
+#cmakedefine PKG_DATADIR "@PKG_DATADIR@"
+#cmakedefine GETTEXT_PACKAGE "@GETTEXT_PACKAGE@"
+#cmakedefine RELEASE_NAME "@RELEASE_NAME@"
+#cmakedefine VERSION "@VERSION@"
+#cmakedefine VERSION_INFO "@VERSION_INFO@"
+
+#endif // CONFIG_H
