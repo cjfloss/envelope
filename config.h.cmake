807944ec714de6b100486122e07943d443cbcb45
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-12-12 19:33:12 -0500

    Add PROGRAM_NAME

--- config.h.cmake
+++ config.h.cmake
@@ -1,6 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#cmakedefine PROGRAM_NAME "@PROGRAM_NAME@"
 #cmakedefine DATADIR "@DATADIR@"
 #cmakedefine PKG_DATADIR "@PKG_DATADIR@"
 #cmakedefine GETTEXT_PACKAGE "@GETTEXT_PACKAGE@"
