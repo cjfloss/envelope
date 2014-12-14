8a4bc6bc83ae30e65576167fb032cb35cb94991a
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-12-14 15:28:28 -0500

    Add sample transactions SQL

---
+++ tests/transactions.sql
@@ -0,0 +1,14 @@
+INSERT INTO transactions
+  VALUES(null, 'Simons',NULL,1,254.12,1,NULL,1418498113);
+
+INSERT INTO transactions
+  VALUES(null, 'HMV','New Pink Floyd CD',1,10.25,1,NULL,1418498113);
+
+INSERT INTO transactions
+  VALUES(null, 'Simons','Suit',1,587.67,1,NULL,1418498113);
+
+INSERT INTO transactions
+  VALUES(null, 'McDonalds',NULL,1,8.46,1,NULL,1418498113);
+
+INSERT INTO transactions
+  VALUES(null, 'Elementary','Bounty pay',0,300.0,1,NULL,1418498113);
